06622dc0ac54d9f9112aad999fbafa501c23339d
Cedric Roux <cedric.roux@eurecom.fr>
2026-08-05 10:57:33 +0200

    use a generic allocator
    Signed-off-by: Cedric Roux <cedric.roux@eurecom.fr>

--- openair2/RRC/NR/rrc_gNB_measurements.c
+++ openair2/RRC/NR/rrc_gNB_measurements.c
@@ -7,75 +7,51 @@
 
 int allocate_measurement_object_id(void *_ue)
 {
-  /* todo(?) use common/utils/collection/linear_alloc.h */
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = 1;
-  int ret;
-  for (ret = 0; ret < 64; ret++, mask <<= 1)
-    if (!(ue->measurement_object_ids & mask))
-      break;
-  DevAssert(ret < 64);
-  ue->measurement_object_ids |= mask;
+  int ret = uid_linear_allocator_64_bits_new(&ue->measurement_object_ids);
+  DevAssert(ret != 64);
   return ret + 1;
 }
 
 void free_measurement_object_id(void *_ue, int mo_id)
 {
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = (uint64_t)1 << (uint64_t)(mo_id - 1);
-  DevAssert(ue->measurement_object_ids & mask);
-  ue->measurement_object_ids &= ~mask;
+  uid_linear_allocator_64_bits_free(&ue->measurement_object_ids, mo_id - 1);
 }
 
 int allocate_measurement_id(void *_ue)
 {
-  /* todo(?) use common/utils/collection/linear_alloc.h */
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = 1;
-  int ret;
-  for (ret = 0; ret < 64; ret++, mask <<= 1)
-    if (!(ue->measurement_ids & mask))
-      break;
-  DevAssert(ret < 64);
-  ue->measurement_ids |= mask;
+  int ret = uid_linear_allocator_64_bits_new(&ue->measurement_ids);
+  DevAssert(ret != 64);
   return ret + 1;
 }
 
 void free_measurement_id(void *_ue, int mo_id)
 {
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = (uint64_t)1 << (uint64_t)(mo_id - 1);
-  DevAssert(ue->measurement_ids & mask);
-  ue->measurement_ids &= ~mask;
+  uid_linear_allocator_64_bits_free(&ue->measurement_ids, mo_id - 1);
 }
 
 int allocate_report_config_id(void *_ue)
 {
-  /* todo(?) use common/utils/collection/linear_alloc.h */
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = 1;
-  int ret;
-  for (ret = 0; ret < 64; ret++, mask <<= 1)
-    if (!(ue->report_config_ids & mask))
-      break;
-  DevAssert(ret < 64);
-  ue->report_config_ids |= mask;
+  int ret = uid_linear_allocator_64_bits_new(&ue->report_config_ids);
+  DevAssert(ret != 64);
   return ret + 1;
 }
 
 void free_report_config_id(void *_ue, int mo_id)
 {
   gNB_RRC_UE_t *ue = _ue;
-  uint64_t mask = (uint64_t)1 << (uint64_t)(mo_id - 1);
-  DevAssert(ue->report_config_ids & mask);
-  ue->report_config_ids &= ~mask;
+  uid_linear_allocator_64_bits_free(&ue->report_config_ids, mo_id - 1);
 }
 
 void reset_all_measurement_ids(void *_ue)
 {
   gNB_RRC_UE_t *ue = _ue;
 
-  ue->measurement_object_ids = 0;
-  ue->measurement_ids = 0;
-  ue->report_config_ids = 0;
+  uid_linear_allocator_64_bits_init(&ue->measurement_object_ids);
+  uid_linear_allocator_64_bits_init(&ue->measurement_ids);
+  uid_linear_allocator_64_bits_init(&ue->report_config_ids);
 }
